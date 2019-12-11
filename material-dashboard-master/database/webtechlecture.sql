56af0158379e7fe8ec906661a00cba708db0be9e
webtek <drev@drev.com>
2019-12-11 19:54:45 +0800

    new dbase

--- material-dashboard-master/database/webtechlecture.sql
+++ material-dashboard-master/database/webtechlecture.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 09, 2019 at 08:33 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 11, 2019 at 11:51 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,15 +25,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `personel_accounts`
+--
+
+DROP TABLE IF EXISTS `personel_accounts`;
+CREATE TABLE IF NOT EXISTS `personel_accounts` (
+  `personel_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `type` enum('Personel','None') NOT NULL DEFAULT 'Personel',
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`personel_ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tenant_accounts`
 --
 
-CREATE TABLE `tenant_accounts` (
-  `tenantID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `tenant_accounts`;
+CREATE TABLE IF NOT EXISTS `tenant_accounts` (
+  `tenantID` int(11) NOT NULL AUTO_INCREMENT,
   `firstName` varchar(50) NOT NULL,
   `lastName` varchar(50) NOT NULL,
-  `roomNumber` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `roomNumber` varchar(11) NOT NULL,
+  PRIMARY KEY (`tenantID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tenant_accounts`
@@ -43,26 +62,6 @@ INSERT INTO `tenant_accounts` (`tenantID`, `firstName`, `lastName`, `roomNumber`
 (1, 'a', 'b2', '32'),
 (2, 'guest', 'g', '45'),
 (3, 'f', 'd', '45');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tenant_accounts`
---
-ALTER TABLE `tenant_accounts`
-  ADD PRIMARY KEY (`tenantID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tenant_accounts`
---
-ALTER TABLE `tenant_accounts`
-  MODIFY `tenantID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
