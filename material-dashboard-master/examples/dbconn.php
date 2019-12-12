cd8a0b058517386a1c19c5e975fc0c6574130d13
Jayditch Balansi <2164482@slu.edu.ph>
2019-12-12 21:42:55 +0800

    login
    login

--- material-dashboard-master/examples/dbconn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = mysqli_connect("localhost", "root", "", "webtechlecture");
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
--- php/login.php
+++ php/login.php
@@ -3,35 +3,27 @@ session_start();
 
 ?>
 <?php
- $conn = mysqli_connect("localhost","root","","webtech_finals");
+ $conn = mysqli_connect("localhost","root","","webtechlecture");
  $role="";
 if (isset($_POST["btnLogin"])) {
 	# code...
-	$email = $_POST["email"];
+	$username = $_POST["username"];
 	$password = $_POST["password"];
-	$query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+	$query = "SELECT * FROM personel_accounts WHERE username='$username' AND password='$password'";
 	$result = mysqli_query($conn, $query);
 	if (mysqli_num_rows($result) > 0) {
 		# code...
 		while ($row = mysqli_fetch_assoc($result)) {
 			# code...
-			if (($row["user_type"] == "Super Admin") && ($row["status"] == "Active")) {
+			if (($row["type"] == "Personel")) {
 				# code...
 				//$_SESSION['LoginUser'] = $row["email"];
 				$_SESSION['login'] = true;
-				header('Location: ../pages/super_admin/dashboard.php');
-			}else if(($row["user_type"] == "Admin")  && ($row["status"] == "Active")){
-				$_SESSION['login'] = true;
-				header('Location: ../pages/admin_user/dashboard.php');
-			}else if(($row["user_type"] == "Client")  && ($row["status"] == "Active")){
-				// $_SESSION['LoginUser'] = $row["email"];
-				// header('Location: user.php');
-				$_SESSION['login'] = true;
-				header('Location: ../pages/client_user/client-home.php');
+				header('Location: ../material-dashboard/pages/dashboard.php');
 			}else{
 				echo '<script type="text/javascript">'; 
 				echo 'alert("Invalid Account!");'; 
-				echo 'window.location.href = "../index.php";';
+				echo 'window.location.href = "../index.html";';
 				echo '</script>';
 
 			}
@@ -40,7 +32,7 @@ if (isset($_POST["btnLogin"])) {
 	}else{
 			echo '<script type="text/javascript">'; 
 			echo 'alert("Invalid Account!");'; 
-			echo 'window.location.href = "../index.php";';
+			echo 'window.location.href = "../index.html";';
 			echo '</script>';
 		
 	}
