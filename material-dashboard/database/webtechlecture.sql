7302b9b5e52886c1adbe824b58be1db907f1a39e
Jayditch Balansi <2164482@slu.edu.ph>
2019-12-12 21:44:11 +0800

    db
    db

--- material-dashboard/database/webtechlecture.sql
+++ material-dashboard/database/webtechlecture.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2019 at 02:34 PM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.2.20
+-- Generation Time: Dec 12, 2019 at 02:43 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `bldginfo`
+--
+
+CREATE TABLE `bldginfo` (
+  `roomID` int(11) NOT NULL,
+  `roomNumber` int(11) NOT NULL,
+  `roomRemarks` varchar(255) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personel_accounts`
+--
+
+CREATE TABLE `personel_accounts` (
+  `personel_ID` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `type` enum('Personel','None') NOT NULL DEFAULT 'Personel',
+  `password` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `personel_accounts`
+--
+
+INSERT INTO `personel_accounts` (`personel_ID`, `username`, `firstName`, `lastName`, `type`, `password`) VALUES
+(9, 'jayditch', 'jayditch', 'balansi', 'Personel', '12345');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tenantinfo`
 --
 
@@ -34,7 +69,7 @@ CREATE TABLE `tenantinfo` (
   `lastName` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `pnumber` varchar(11) NOT NULL,
+  `pnumber` int(11) NOT NULL,
   `roomNumber` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -43,15 +78,62 @@ CREATE TABLE `tenantinfo` (
 --
 
 INSERT INTO `tenantinfo` (`tenantID`, `firstName`, `lastName`, `address`, `email`, `pnumber`, `roomNumber`) VALUES
-(1, 'Patrick', 'Responde', 'bukaneg street, legarda, baguio city', '2152751@slu.edu.ph', '1234567891', '211'),
-(2, 'Jayditch', 'Balansi', 'Tuding, Baguio City', 'jayditch@email.com', '1987654321', '212'),
-(3, 'Anne', 'Manahan', 'Goshen, Bakakeng, Baguio City', '2164165@slu.edu.ph', '1234569871', '213'),
-(4, 'Destine', 'Aldana', 'San Luis Baguio City', 'aldana@email.com', '09123658746', '214'),
-(5, 'Rico', 'Pangan', 'Baguio City', 'pangan@email.com', '09846325176', '215');
+(1, 'patrickasd', 'responde', 'bukaneg street, legarda, baguio city', '2164165@slu.edu.ph', 999999999, '3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenantreq`
+--
+
+CREATE TABLE `tenantreq` (
+  `tenantID` int(11) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `pnumber` int(11) NOT NULL,
+  `reqTenant` varchar(255) NOT NULL,
+  `roomNumber` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tenantreq`
+--
+
+INSERT INTO `tenantreq` (`tenantID`, `firstName`, `lastName`, `pnumber`, `reqTenant`, `roomNumber`) VALUES
+(1, 'patrick', 'responde', 9999999, 'usto ko ng hakdog', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenant_accounts`
+--
+
+CREATE TABLE `tenant_accounts` (
+  `tenantID` int(11) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `roomNumber` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tenant_accounts`
+--
+
+INSERT INTO `tenant_accounts` (`tenantID`, `firstName`, `lastName`, `roomNumber`) VALUES
+(1, 'a', 'b2', '32'),
+(2, 'guest', 'g', '45'),
+(3, 'f', 'd', '45'),
+(4, 'Patrick', 'Responde', '512');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `personel_accounts`
+--
+ALTER TABLE `personel_accounts`
+  ADD PRIMARY KEY (`personel_ID`);
 
 --
 -- Indexes for table `tenantinfo`
@@ -60,14 +142,26 @@ ALTER TABLE `tenantinfo`
   ADD PRIMARY KEY (`tenantID`);
 
 --
+-- Indexes for table `tenant_accounts`
+--
+ALTER TABLE `tenant_accounts`
+  ADD PRIMARY KEY (`tenantID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tenantinfo`
+-- AUTO_INCREMENT for table `personel_accounts`
 --
-ALTER TABLE `tenantinfo`
-  MODIFY `tenantID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `personel_accounts`
+  MODIFY `personel_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tenant_accounts`
+--
+ALTER TABLE `tenant_accounts`
+  MODIFY `tenantID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
